58c7ffc7579bec0d8836aceb45d1d002f32aeae0
krishnasarkar5 <krishnasarkar95@gmail.com>
2023-05-15 08:00:27 +0530

    added concurrent hash map

---
+++ src/main/java/concuurentCollection/ConcurrentHashMapDemo.java
@@ -0,0 +1,29 @@
+package concuurentCollection;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapDemo {
+
+
+    public static void main(String[] args) {
+
+
+        ConcurrentHashMap<Integer,String> chm = new ConcurrentHashMap<>();
+
+        chm.put(101,"A");
+        chm.put(102,"B");
+        chm.put(103,"C");
+        chm.put(104,"F");// this  put methods are from map interface so there are same like in map
+        chm.put(104,"G");   // it will replace the last value of 104 key
+        chm.putIfAbsent(108,"D"); // this entry will be added in chm as 108 key is not present
+        chm.putIfAbsent(101,"D"); // as key 101 already present do it will bot be added in chm
+        chm.remove(101,"D"); // successfully removed as given key and corresponding value is matched
+        chm.replace(102,"B","E"); // successfully replaced as given key and corresponding old value is matched
+        chm.replace(103,"M"); //
+        System.out.println(chm); // this  replace  methods are from map interface so there are same like in map
+
+
+        //o/p =>  {101=A, 102=E, 103=M, 104=G, 108=D}
+
+    }
+}
